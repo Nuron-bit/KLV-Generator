ca1ab6b547a71ceaf06b868145ad26a888a203bd
Nuron <54858219+Nuron-bit@users.noreply.github.com>
2023-06-02 19:55:23 +0300

    4.27.cpp

--- KLV.cpp
+++ KLV.cpp
@@ -4,35 +4,52 @@
 #include <openssl/evp.h>
 
 const int PROTOCOL = 191;
-const std::string GAME_VERSION = "4.25";
+const std::string GAME_VERSION = "4.27";
 
-std::string MD5(const std::string& input) {
-	unsigned char digest[EVP_MAX_MD_SIZE];
-	unsigned int digest_len;
-	EVP_MD_CTX* ctx = EVP_MD_CTX_new();
-	EVP_DigestInit_ex(ctx, EVP_md5(), nullptr);
-	EVP_DigestUpdate(ctx, input.c_str(), input.length());
-	EVP_DigestFinal_ex(ctx, digest, &digest_len);
-	EVP_MD_CTX_free(ctx);
+namespace hash {
+	std::string MD5(const std::string& input) {
+		unsigned char digest[EVP_MAX_MD_SIZE];
+		unsigned int digest_len;
+		EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+		EVP_DigestInit_ex(ctx, EVP_md5(), nullptr);
+		EVP_DigestUpdate(ctx, input.c_str(), input.length());
+		EVP_DigestFinal_ex(ctx, digest, &digest_len);
+		EVP_MD_CTX_free(ctx);
 
-	char md5string[33];
-	for (int i = 0; i < 16; i++) {
-		sprintf(&md5string[i * 2], "%02x", (unsigned int)digest[i]);
+		char md5string[33];
+		for (int i = 0; i < 16; i++) {
+			sprintf(&md5string[i * 2], "%02x", (unsigned int)digest[i]);
+		}
+		md5string[32] = '\0';
+
+		std::string hash = std::string(md5string);
+		std::transform(hash.begin(), hash.end(), hash.begin(), ::toupper);
+		return hash;
 	}
-	md5string[32] = '\0';
-
-	std::string hash = std::string(md5string);
-	std::transform(hash.begin(), hash.end(), hash.begin(), ::toupper);
-	return hash;
 }
 
-std::string GenerateKLV(const int protocol, const int hash, const std::string& version, const std::string& rid) {
-	const static std::string keys[] = {
-        "13c93f386db9da3e00dda16d770b0c83",
-        "6b1c01f9128a62a2c97b1a0da4612168",
-        "3402d278d8519a522c94d122e98e2e49",
-        "ba95613bc0fd94a9d89c5919670e7d5d"
-   	};
-	
-	return MD5(version + keys[0] + std::to_string(protocol) + keys[1] + std::to_string(hash) + keys[2] + rid + keys[3]);
+namespace proton {
+    std::string generate_klv(const int protocol, const std::string& version, const std::string& rid) {
+        constexpr std::array salts = {
+            "e9fc40ec08f9ea6393f59c65e37f750aacddf68490c4f92d0d2523a5bc02ea63",
+            "c85df9056ee603b849a93e1ebab5dd5f66e1fb8b2f4a8caef8d13b9f9e013fa4",
+            "3ca373dffbf463bb337e0fd768a2f395b8e417475438916506c721551f32038d",
+            "73eff5914c61a20a71ada81a6fc7780700fb1c0285659b4899bc172a24c14fc1"
+        };
+
+        static std::array constant_values = {
+            hash::sha256(hash::md5(hash::sha256(std::to_string(protocol)))),
+            hash::sha256(hash::sha256(version)),
+            hash::sha256(hash::sha256(std::to_string(protocol)) + salts[3])
+        };
+
+        return hash::sha256(constant_values[0]
+            + salts[0]
+            + constant_values[1]
+            + salts[1]
+            + hash::sha256(hash::md5(hash::sha256(rid)))
+            + salts[2]
+            + constant_values[2]
+        );
+    }
 }
